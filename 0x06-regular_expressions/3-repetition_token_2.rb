6ac8b04c04ae7a26f01e77ef0700fa27bb1544d1
abderrahmane-elrhassani <abdoghessan@example.com>
2024-02-27 14:37:46 -0800

    2. Repetition Token #2

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# This script takes a single argument and applies it to a regular expression
+# matching method.
+# The regular expression should target the specified cases.
+
+
+puts ARGV[0].scan(/hbt+n/).join
